6dd6c6045bd6b6e6df65a7cba55fec48425a434d
Sally O'Malley <somalley@redhat.com>
2019-02-27 15:48:46 -0500

    bug 1683058: Prevent sharing of configmap and secret data
    This PR updates handling of servingcert secrets, cabundle-injection configmaps
    so that invalid modifications of either cause a regeneration, and invalid data
    is removed.  This PR has refactored code in secret_updating_controller and
    adds e2e tests.

--- pkg/controller/configmapcainjector/controller/configmap_injecting_controller.go
+++ pkg/controller/configmapcainjector/controller/configmap_injecting_controller.go
@@ -46,20 +46,21 @@ func (ic *configMapCABundleInjectionController) Sync(obj v1.Object) error {
 	if !api.HasInjectCABundleAnnotation(sharedConfigMap) {
 		return nil
 	}
+	// ensure data of configmap
+	return ic.ensureConfigMapCABundleInjection(sharedConfigMap)
+}
+
+// ensureConfigMapCABundleInjection will create or update configmap for the
+// CA bundle injection as appropriate.
+func (ic *configMapCABundleInjectionController) ensureConfigMapCABundleInjection(current *corev1.ConfigMap) error {
 	// skip updating when the CA bundle is already there
-	if data, ok := sharedConfigMap.Data[api.InjectionDataKey]; ok && data == ic.ca {
+	if data, ok := current.Data[api.InjectionDataKey]; ok &&
+		data == ic.ca && len(current.Data) == 1 {
 		return nil
 	}
-
 	// make a copy to avoid mutating cache state
-	configMapCopy := sharedConfigMap.DeepCopy()
-
-	if configMapCopy.Data == nil {
-		configMapCopy.Data = map[string]string{}
-	}
-
-	configMapCopy.Data[api.InjectionDataKey] = ic.ca
-
-	_, err := ic.configMapClient.ConfigMaps(configMapCopy.Namespace).Update(configMapCopy)
+	configMapCopy := current.DeepCopy()
+	configMapCopy.Data = map[string]string{api.InjectionDataKey: ic.ca}
+	_, err := ic.configMapClient.ConfigMaps(current.Namespace).Update(configMapCopy)
 	return err
 }
