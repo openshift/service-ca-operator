abe8f99bce55933427bb1a25beafaf17aa1fd72e
Stanislav Laznicka <slaznick@redhat.com>
2020-08-12 16:42:37 +0200

    remove mo's operator from deps
    🐼

--- vendor/github.com/openshift/operator-boilerplate-legacy/pkg/operator/filter.go
+++
@@ -1,7 +0,0 @@
-package operator
-
-import "github.com/openshift/operator-boilerplate-legacy/pkg/controller"
-
-func FilterByNames(names ...string) controller.Filter {
-	return controller.FilterByNames(nil, names...)
-}
