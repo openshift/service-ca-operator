bea731bd8f2ac45f02f325e3b27c963898735830
wangke19 <kewang@redhat.com>
2025-10-28 22:32:52 +0800

    Fix gofmt formatting in constants.go
    - Run make update-gofmt to fix formatting issues
    - Ensures code passes verify-gofmt check

--- test/tests-extension/util/constants.go
+++ test/tests-extension/util/constants.go
@@ -6,16 +6,16 @@ package util
 const (
 	// InjectionDataKey is the key used to identify the CA bundle in the injection
 	InjectionDataKey = "service-ca.crt"
-	
+
 	// SigningCABundleConfigMapName is the name of the signing CA bundle configmap
 	SigningCABundleConfigMapName = "signing-cabundle"
-	
+
 	// BundleDataKey is the key used to identify the CA bundle in the signing secret
 	BundleDataKey = "ca-bundle.crt"
-	
+
 	// InjectCABundleAnnotationName is the annotation name for CA bundle injection
 	InjectCABundleAnnotationName = "service.beta.openshift.io/inject-cabundle"
-	
+
 	// ServingCertSecretAnnotation stores the name of the secret to generate into
 	ServingCertSecretAnnotation = "service.beta.openshift.io/serving-cert-secret-name"
 )
