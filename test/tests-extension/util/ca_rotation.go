16bcd0deb6c60e67f6e6904369a2f171e3c5b70e
wangke19 <kewang@redhat.com>
2025-10-29 14:41:36 +0800

    Fix error handling in PollForCARecreation to distinguish NotFound from other errors
    - Add apierrors import to ca_rotation.go
    - Modify PollForCARecreation to use errors.IsNotFound(err) to distinguish
      NotFound errors (retryable) from other errors (abort polling)
    - This prevents infinite polling on non-retryable errors like permission denied
    
    Fixes error handling issue identified in PR review.

--- test/tests-extension/util/ca_rotation.go
+++ test/tests-extension/util/ca_rotation.go
@@ -6,6 +6,7 @@ import (
 	"time"
 
 	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/util/wait"
 	"k8s.io/client-go/kubernetes"
@@ -24,7 +25,10 @@ func PollForCARecreation(client kubernetes.Interface) error {
 	return wait.PollImmediate(time.Second, rotationPollTimeout, func() (bool, error) {
 		_, err := client.CoreV1().Secrets(serviceCAControllerNamespace).Get(context.TODO(), signingKeySecretName, metav1.GetOptions{})
 		if err != nil {
-			return false, nil
+			if errors.IsNotFound(err) {
+				return false, nil
+			}
+			return false, err
 		}
 		return true, nil
 	})
