1d5fcdcd803d61fa63040290533916c1e6a538be
Brandon Palm <bpalm@redhat.com>
2025-11-24 14:10:47 -0600

    Migrate away from deprecated ioutil

--- pkg/controller/servingcert/starter/starter.go
+++ pkg/controller/servingcert/starter/starter.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"crypto/x509"
 	"fmt"
-	"io/ioutil"
 	"os"
 	"time"
 
@@ -117,7 +116,7 @@ func StartServiceServingCertSigner(ctx context.Context, controllerContext *contr
 // bridge trust between the current and previous CA, but a new cluster
 // will not have a previous CA.
 func readIntermediateCACert(filename string) (*x509.Certificate, error) {
-	certsPEMBlock, err := ioutil.ReadFile(filename)
+	certsPEMBlock, err := os.ReadFile(filename)
 	if os.IsNotExist(err) {
 		klog.V(4).Infof("%q does not exist which indicates that an intermediate certificate was not specified", filename)
 		return nil, nil
